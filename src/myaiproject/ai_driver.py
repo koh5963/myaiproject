befd71c5c55681461c1e47ad0f085f66b3ad04a6
koh5963 <kotaro11140828@gmail.com>
2025-06-25 00:03:21 +0900

    要件をユーザー入力に変更

--- src/myaiproject/ai_driver.py
+++ src/myaiproject/ai_driver.py
@@ -9,18 +9,19 @@ def connect() -> ChatOpenAI :
         model_name="llama-3-8b-instruct-gradient-1048k"  # LM Studioでのモデル名に合わせて調整
     )
 
-def get_ai_response() -> str :
-    input = "フルパスで入力されたCSVファイルを読み込んで、TSVファイルに変換して入力CSVと同じフォルダに出力するツールを作成したい。"
+def get_ai_response(user_input: str) -> str :
+    ## input = "ユーザーが入力したパスのCSVファイルを読み込んで、TSVファイルに変換して入力CSVと同じフォルダに出力するツールを作成したい。出力するファイルパスのボリュームが実行ボリュームと異なる場合、ボリュームを移動してから出力すること。"
     template = PromptTemplate(
         input_variables=["user_requirement"],
         template="""
+        あなたはコーディングのプロフェッショナルです。
         以下の要件に基づいて、Pythonで動作するシンプルなスクリプトを生成してください。
-        要件: {user_requirement}
+        出力はコードのみで、説明や補足は絶対に出力しないでください。
 
-        出力はコードのみで、説明や補足は不要です。
+        要件: {user_requirement}
         """
     )
-    prompt = template.format(user_requirement=input)
+    prompt = template.format(user_requirement=user_input)
 
     llm = connect()
     response = llm.invoke([HumanMessage(content=prompt)])
--- src/myaiproject/myaiproject.py
+++ src/myaiproject/myaiproject.py
@@ -2,5 +2,9 @@
 from myaiproject import ai_driver, pyfile_creator
 
 def create_code():
-    response = ai_driver.get_ai_response()
+    user_requirements = input("Please enter your requirements... : ")
+    if len(user_requirements) == 0:
+        return
+
+    response = ai_driver.get_ai_response(user_requirements)
     pyfile_creator.create_new(response)
